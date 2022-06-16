677c108d8d816fbae4333b3d3a39bd2ff2a23cfa
Josh Snyder <joshrsnyder@gmail.com>
2022-06-16 14:27:39 -0500

    completed customer info form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import CustomerInfo from '../CustomerInfo/CustomerInfo'
 
 // Import the Router to allow for linking throughout out App.jsx project
 // and Route to link to specific pages
@@ -17,7 +18,7 @@ function App() {
     
         <img src='images/pizza_photo.png' />
         <p>Pizza is great.</p>
-    
+      <CustomerInfo/>
       </div>
     </Router>
   );
